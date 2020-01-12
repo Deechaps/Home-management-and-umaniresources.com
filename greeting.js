1a74e5ebe9f25828d05a4728ab9fafafff495c4e
Deechaps <59717477+Deechaps@users.noreply.github.com>
2020-01-11 18:34:47 -0600

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,10 +1,10 @@
 var name = prompt("Hi, Nice to see you here. What's your name?");
 if (name === "Mani") {
-    alert ("Hi, Mani Bro! Whats up!!");
+    alert ("Hey, Mani Bro!!! Whats up?? Lets calculate this month expenses before opening umaniresources.com!!");
 } else if (name === "Uma") { 
-    alert ("Hi, Uma Vauju! How are you?");
+    alert ("Hi, Uma Vauju! How are you? Lets calculate this month expenses before opening umaniresources.com!!");
 } else { 
-     alert("Humm you must be Mani bro's relative!! Nice to see you here");
+     alert("Humm you must be Mani bro's relative!! Nice to see you here, Lets calculate this month expenses before opening umaniresources.com");
        }
 
 
