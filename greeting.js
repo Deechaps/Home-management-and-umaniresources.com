eccceba3eb880b89089bd99987011a347e737058
Deechaps <59717477+Deechaps@users.noreply.github.com>
2020-01-11 18:28:15 -0600

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,10 @@
+var name = prompt("Hi, Nice to see you here. What's your name?");
+if (name === "Mani") {
+    alert ("Hi, Mani Bro! Whats up!!");
+} else if (name === "Uma") { 
+    alert ("Hi, Uma Vauju! How are you?");
+} else { 
+     alert("Humm you must be Mani bro's relative!! Nice to see you here");
+       }
+
+
